bdb1e8be4c606aace0b48751b575b5251ea89775
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-12 19:47:24 -0300

    fix: update heroku.yml

--- jungle_news/settings.py
+++ jungle_news/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
-DEBUG = int(os.environ.get('DEBUG', default=1))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jungle-news']
 
