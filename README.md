118518f3149d35466c0e6e5f2a8dd4cf29032042
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-12 22:42:41 -0300

    feat: jwt authentication

---
+++ README.md
@@ -0,0 +1,7 @@
+### Run application in development mode
+* Install Docker Compose following the tutorial: https://docs.docker.com/compose/install/
+* Build image: `docker-compose build`
+* Start services: `docker-compose up -d`
+* Run collectstatic: `docker-compose exec web python manage.py collectstatic --noinput`
+* Run migrations: `docker-compose exec web python manage.py migrate --noinput`
+* Create Django admin user: `docker-compose exec web python manage.py createsuperuser`
---
+++ jungle_news/api/urls.py
@@ -0,0 +1,36 @@
+"""jungle_news URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+app_name = 'api'
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Jugle News API",
+        default_version='v1',
+        contact=openapi.Contact(email="emanoeli.madalosso@gmail.com"),
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path('swagger.<slug:format>)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('authentication.urls')),
+]
