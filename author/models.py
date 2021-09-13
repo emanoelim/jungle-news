fb2877d6502c353c9d15219bb80c42be4cd540fb
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-13 08:46:22 -0300

    feat: author CRUD

---
+++ author/models.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
