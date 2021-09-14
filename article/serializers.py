e7bd871f01c751e2433f10334c00b0defad319ef
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-14 08:31:16 -0300

    feat: first paragraph article serializer

--- article/serializers.py
+++ article/serializers.py
@@ -14,3 +14,16 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    first_paragraph = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+        depth = 1
+
+    @staticmethod
+    def get_first_paragraph(instance):
+        return instance.body.split('.')[0]
--- article/views.py
+++ article/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
 from article.filters import ArticleFilterSet
-from article.serializers import ArticleSerializer, ArticleCreateSerializer
+from article.serializers import ArticleSerializer, ArticleCreateSerializer, ArticleListSerializer
 from article.models import Article
 
 
@@ -21,6 +21,6 @@ class ArticleView(ModelViewSet):
 class ArticleListView(ModelViewSet):
     http_method_names = ['get']
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    serializer_class = ArticleListSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ArticleFilterSet
