fa2090b72bfff267c3c0e3794e3736fb8dda7003
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-16 11:56:15 -0300

    feat: image upload

--- author/models.py
+++ author/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Author(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=250)
+    picture = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return self.name
