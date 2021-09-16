9d3a8d7f26ff0293504fba947f763d908efd8158
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-15 21:31:02 -0300

    feat: sign-up

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from authentication.views import SignUpView
+
 urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('sign-up/', SignUpView.as_view(), name='sign_up'),
 ]
